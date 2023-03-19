04f7e6f2ed1eb4c4ed68942daed90bc2a9578c3c
Prasad Vennam <vennamprasad@gmail.com>
2023-03-19 22:18:05 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+
+plugins {
+	id("org.springframework.boot") version "3.0.4"
+	id("io.spring.dependency-management") version "1.1.0"
+	kotlin("jvm") version "1.7.22"
+	kotlin("plugin.spring") version "1.7.22"
+	kotlin("plugin.jpa") version "1.7.22"
+}
+
+group = "com.koltinspring"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	//logging
+	implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
+	testImplementation("io.mockk:mockk:1.10.4")
+	testImplementation("com.ninja-squad:springmockk:3.0.1")
+
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+sourceSets {
+	test {
+		java {
+			setSrcDirs(listOf("src/test/intg",  "src/test/unit"))
+		}
+	}
+
+	/*test {
+        withConvention(KotlinSourceSet::class) {
+            kotlin.srcDir(listOf("src/test/intg", "src/test/unit"))
+        }
+    }*/
+
+}
---
+++ src/main/kotlin/com/koltinspring/controller/GreetingController.kt
@@ -0,0 +1,22 @@
+package com.koltinspring.controller
+import com.koltinspring.service.GreetingService
+import mu.KLogging
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/greetings")
+class GreetingController(val greetingService: GreetingService) {
+
+    companion object : KLogging()
+    @GetMapping("/{name}")
+    fun retrieveGreeting(@PathVariable("name") name : String): String {
+        logger.info("Name is : $name")
+        return greetingService.retrieveGreeting(name)
+    }
+    /*: String {
+        return "Hello $name"
+    }*/
+}
